31e6264f5f4b4728d126ea8beb3d825097255999
Yarcrazy <yacrazy@yandex.ru>
2020-04-21 00:26:07 +0700

    lesson 1 кроме перерендера при клике

---
+++ Ivan_Tatarinov/src/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+const messages = ['1', '2'];
+
+const Button = () => {
+  const handleClick = () => {
+    messages.push('норм');
+    console.log(messages);
+  };
+  return <button onClick={handleClick}>Press</button>
+};
+
+const MessageList = (props) => {
+  return props.messages.map((item, index) => <Message text={item} key={index}/>)
+};
+
+const Message = (props) => {
+  return <div>Сообщение: {props.text}</div>;
+};
+
+ReactDom.render(
+  <React.Fragment>
+    <Button/>
+    <MessageList messages={messages}/>
+  </React.Fragment>,
+  document.getElementById('root'),
+);
