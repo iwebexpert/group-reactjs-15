c502e52f81f9baddecf6ca210cb715985ee51afa
yarcrazy <yarcrazy@yandex.ru>
2020-05-02 11:43:22 +1400

    Бот отвечает только на последнее сообщение

--- Ivan_Tatarinov/src/components/Messenger/Messenger.jsx
+++ Ivan_Tatarinov/src/components/Messenger/Messenger.jsx
@@ -5,42 +5,41 @@ import {MessageList} from "components/MessageList";
 import './Messanger.scss';
 
 export class Messenger extends React.Component {
-  state = {
-    messages: [
-      {
-        text: 'Сообщение 1',
-        author: 'Ivan',
-      }
-    ],
-  };
-
-  handleMessageSend = (message) => {
-    this.setState((prevState) => {
-      return {messages: [...prevState.messages, message]}
-    })
-  };
-
-  componentDidUpdate(prevProps, prevState, snapshot) {
-    const message = {
-      text: 'This is a service message',
-      author: 'Bot',
+    state = {
+        messages: [
+            {
+                text: 'Сообщение 1',
+                author: 'Ivan',
+            }
+        ],
     };
-    const lastMessage = this.state.messages.slice(-1)[0];
-    if (lastMessage.author !== message.author) {
-      console.log(this.state.messages);
-      setTimeout(() => {
-        this.setState({messages: this.state.messages.concat(message)})
-      }, 1000)
-    }
-  }
 
-  render() {
-    const {messages} = this.state;
-    return (
-      <div className='messanger'>
-        <MessageList messages={messages}/>
-        <MessageField onSend={this.handleMessageSend}/>
-      </div>
-    );
-  }
+    handleMessageSend = (message) => {
+        this.setState({messages: this.state.messages.concat(message)})
+    };
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        const message = {
+            text: 'This is a service message',
+            author: 'Bot',
+        };
+
+        setTimeout(() => {
+            const lastMessage = this.state.messages.slice(-1)[0];
+            if (lastMessage.author !== message.author) {
+                this.setState({messages: this.state.messages.concat(message)})
+            }
+        }, 1000)
+
+    }
+
+    render() {
+        const {messages} = this.state;
+        return (
+            <div className='messanger'>
+                <MessageList messages={messages}/>
+                <MessageField onSend={this.handleMessageSend}/>
+            </div>
+        );
+    }
 }
