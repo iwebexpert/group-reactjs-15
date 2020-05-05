4dc9b06f43288f9a35ea4ad72d566f4a8bab0fda
Boris Aniskevich <aniskevich.bo@gmail.com>
2020-05-06 00:12:58 +1000

    srv refactoring

--- Boris_Aniskevich/src/store/store.js
+++ Boris_Aniskevich/src/store/store.js
@@ -1,10 +1,12 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux'
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
 import { reducer as formReducer } from 'redux-form'
 
 import chatReducer from 'reducers/chatReducer'
 import messageReducer from 'reducers/messageReducer'
 import userReducer from 'reducers/userReducer'
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
 const reducers = combineReducers({
     chat: chatReducer,
@@ -13,4 +15,4 @@ const reducers = combineReducers({
     user: userReducer,
 })
 
-export const store = createStore(reducers, applyMiddleware(thunk))
+export const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)))
