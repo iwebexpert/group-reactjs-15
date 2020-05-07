99c8c6ddc4345b588601ef7dbed56bb2f347356a
yarcrazy <yarcrazy@yandex.ru>
2020-05-08 01:11:05 +0700

    L5 Ivan Tatarinov

--- Ivan_Tatarinov/src/components/Messenger/Messenger.jsx
+++ Ivan_Tatarinov/src/components/Messenger/Messenger.jsx
@@ -4,98 +4,26 @@ import {MessageField} from 'components/MessageField';
 import {MessageList} from "components/MessageList";
 import {List, ListItem, ListItemText} from "@material-ui/core";
 import {Link} from "react-router-dom";
-import './Messanger.scss';
+import './Messenger.scss';
 
 export class Messenger extends React.Component {
-    state = {
-        chats: [
-            {
-                name: 'Chat 1',
-                messages: [
-                    {
-                        text: 'Текстовое сообщение 1',
-                        author: 'Ivan',
-                    }
-                ],
-            },
-            {
-                name: 'Chat 2',
-                messages: [
-                    {
-                        text: 'Текстовое сообщение 2',
-                        author: 'Ivan',
-                    }
-                ],
-            },
-            {
-                name: 'Chat 3',
-                messages: [
-                    {
-                        text: 'Текстовое сообщение 3',
-                        author: 'Ivan',
-                    }
-                ],
-            },
-
-            //TODO - с помощью setState добавлять новые чаты
-        ],
-    };
-
-    handleMessageSend = (message) => {
-        const {chats} = this.state;
-        const {match} = this.props;
-        chats[match.params.id - 1].messages = this.messages.concat([message]);
-
-        this.setState({
-            chats: chats
-        });
-    }
-
-    componentDidUpdate(prevProps, prevState, snapshot) {
-        const botMessage = {
-            text: 'This is a service message',
-            author: 'Bot',
-        };
-
-        if (this.messages.length) {
-            setTimeout(() => {
-                const {author} = this.messages[this.messages.length - 1];
-                if (author !== botMessage.author) {
-                    this.handleMessageSend(botMessage);
-                }
-            }, 1000)
-        }
-    }
-
-    get messages() {
-        const {chats} = this.state;
-        const {match} = this.props;
-        let messages = null;
-        if (match && chats[+match.params.id - 1]) {
-            messages = chats[+match.params.id - 1].messages;
-        }
-
-        return messages;
-    }
-
     render() {
-        const {chats} = this.state;
-        return (
-            <div className='container'>
-                <List>
-                    {chats.map((item, index) => (
-                        <ListItem key={index}>
-                            <Link to={'/chats/' + (index + 1)}>
-                                <ListItemText primary={item.name}/>
-                            </Link>
-                        </ListItem>))}
+        const {chats, messages, sendMessage} = this.props;
 
+        return (
+            <React.Fragment>
+                <List>
+                    {chats.map((chat, index) => <ListItem key={index}>
+                        <Link to={chat.link}>
+                            <ListItemText primary={chat.name}/>
+                        </Link>
+                    </ListItem>)}
                 </List>
-                <div>
-                    {this.messages ? <MessageList messages={this.messages}/> : 'Выберите чат!'}
-                    {this.messages && <MessageField onSend={this.handleMessageSend}/>}
+                <div className="messenger">
+                    {messages ? <MessageList messages={messages}/> : 'Пожалуйста, выберите чат'}
+                    {messages && <MessageField onSend={sendMessage}/>}
                 </div>
-            </div>
+            </React.Fragment>
         );
     }
 }
