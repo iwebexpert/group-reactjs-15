f465bd36de2f340b1b3dec80f25d0be6b34b5560
a6cexz <lynxsm@gmail.com>
2020-05-08 11:26:08 +0300

    hw6

--- Alex_Zakharov/src/AppRouting.jsx
+++ Alex_Zakharov/src/AppRouting.jsx
@@ -1,22 +1,29 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import { Switch, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { ConnectedRouter } from 'connected-react-router';
 
 import { routes } from './routes';
-import { store } from './store';
+import initStore, { history } from './store';
+
+const { store, persistor } = initStore();
 
 export class AppRouting extends React.Component {
     render() {
         return <Provider store={store}>
-            <BrowserRouter>
-                <Switch>
-                    {
-                        routes.map((route, index) =>
-                            <Route key={index} {...route} />)
-                    }
-                </Switch>
-            </BrowserRouter>
+            <PersistGate loading={null} persistor={persistor}>
+                <ConnectedRouter history={history}>
+                    <Switch>
+                        {
+                            routes.map((route, index) =>
+                                <Route key={index} {...route} />)
+                        }
+                    </Switch>
+                </ConnectedRouter>
+            </PersistGate>
         </Provider>;
     }
 }
