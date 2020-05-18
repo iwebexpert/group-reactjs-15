ccc3ad2a546fbf9a1ae220691a5edfcb3180ee00
Boris Aniskevich <aniskevich.bo@gmail.com>
2020-05-19 02:59:35 +1000

    restyling header

--- Boris_Aniskevich/src/components/Header/Header.jsx
+++ Boris_Aniskevich/src/components/Header/Header.jsx
@@ -6,15 +6,12 @@ import style from './Header.module.scss'
 const Header = props => {
     return (
         <div className={style.header}>
-            <div className={style.controls}>
-                <div className={style.welcome}>Welcome, {props.username}</div>
                 {
                     props.location === '/profile'
                     ? <button className={style.button}><Link to='/chat'>Chats</Link></button>
                     : <button className={style.button}><Link to='/profile'>Profile</Link></button>
                 }
                 <button onClick={props.logout} className={style.button}>Log out</button>
-            </div>
         </div>
     )
 }
